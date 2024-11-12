3fb13270b2e0d0b2ddbf7f6df7a2b6ed46647ada
Ziqun Liu <ziqun.liu@yahoo.com>
2024-11-12 00:43:33 -0800

    LC310 Minimum Height Trees

--- LC310MinimumHeightTrees/main.cpp
+++ LC310MinimumHeightTrees/main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> findMinHeightTrees(const int &n, vector<vector<int>>& edges) {
+    vector<int> findMinHeightTrees(const int &n, vector<vector<int>> &edges) {
         if (n == 1) return {0}; // One node has no edge
-        vector<set<int>> adjacencySets(n);
-        for (const vector<int> &edge : edges) {
+        unordered_map<int, unordered_set<int>> adjacencySets;
+        for (const vector<int> &edge: edges) {
             adjacencySets[edge[0]].insert(edge[1]);
             adjacencySets[edge[1]].insert(edge[0]);
         }
 
-        vector<int> leafNodes, newLeafNodes;
+        vector<int> leafNodes;
         // Find leaf objects and place them in leafNodes array
-        for (int i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             if (adjacencySets[i].size() == 1)
                 leafNodes.push_back(i);
         }
 
-        int numberOfNodes = n;
-        while (numberOfNodes > 2) {
-            numberOfNodes -= leafNodes.size(); // Deduce the number of leaf nodes
+        int remainingNodes = n;
+        while (remainingNodes > 2) {
+            remainingNodes -= leafNodes.size(); // Deduct the number of leaf nodes
+            vector<int> newLeafNodes;
             // Now traverse the leaf nodes
-            for (const int &leafIndex : leafNodes) {
+            for (const int &leafIndex: leafNodes) {
                 // Traverse each integer element in leafIndex's corresponding adjacency set
-                for (const int &adjNum : adjacencySets[leafIndex]) {
+                for (const int &adjNum: adjacencySets[leafIndex]) {
                     // For each integer in the set,
                     // remove leafIndex from this integer's corresponding set.
                     // Note that here adjNum is the index and the leafIndex is the element
@@ -41,8 +42,8 @@ public:
             leafNodes = move(newLeafNodes);
         } // End while
         return leafNodes;
-    } // Time complexity O()
-      // Space complexity O()
+    } // Time complexity O(V + E)
+      // Space complexity O(V + E)
 };
 
 int main() {
