6e111c88ddccf1b62f754e0cf29cee771db816a6
Ziqun Liu <ziqun.liu@yahoo.com>
2024-11-11 23:59:21 -0800

    LC310 Minimum Height Trees C++

--- LC310MinimumHeightTrees/main.cpp
+++ LC310MinimumHeightTrees/main.cpp
@@ -7,28 +7,44 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        vector<int> result;
+    vector<int> findMinHeightTrees(const int &n, vector<vector<int>>& edges) {
+        vector<int> leafNodes, newLeafNodes;
         if (edges.empty() || !edges.size()) {
-            result.push_back(0);
-            return result;
+            leafNodes.push_back(0);
+            return leafNodes;
         }
 
-        vector<set<int>> adjacencyArray;
+        vector<set<int>> adjacencySets;
         for (const vector<int> &edge : edges) {
-            adjacencyArray[edge[0]].insert(edge[1]);
-            adjacencyArray[edge[1]].insert(edge[0]);
+            adjacencySets[edge[0]].insert(edge[1]);
+            adjacencySets[edge[1]].insert(edge[0]);
         }
 
-        // Find leaf objects
+        // Find leaf objects and place them in leafNodes array
         for (int i=0; i<n; ++i) {
-            if (adjacencyArray[i].size() == 1)
-                result.push_back(i);
+            if (adjacencySets[i].size() == 1)
+                leafNodes.push_back(i);
         }
 
-        while (n > 2) {
-
-        }
+        int numberOfNodes = n;
+        while (numberOfNodes > 2) {
+            numberOfNodes -= leafNodes.size(); // Deduce the number of leaf nodes
+            // Now traverse the leaf nodes
+            for (const int &leafIndex : leafNodes) {
+                // Traverse each integer element in leafIndex's corresponding adjacency set
+                for (const int &adjNum : adjacencySets[leafIndex]) {
+                    // For each integer in the set,
+                    // remove leafIndex from this integer's corresponding set.
+                    // Note that here adjNum is the index and the leafIndex is the element
+                    // to be removed from adjNum's corresponding adjacency set
+                    adjacencySets[adjNum].erase(leafIndex);
+                    if (adjacencySets[adjNum].size() == 1)
+                        newLeafNodes.push_back(adjNum);
+                }
+            }
+            leafNodes = newLeafNodes;
+        } // End while
+        return leafNodes;
     }
 };
 
