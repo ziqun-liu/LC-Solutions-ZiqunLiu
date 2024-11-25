79d56df9827c665eaaa34d2c2933efe79012c13b
Ziqun Liu <ziqun.liu@yahoo.com>
2024-11-24 23:23:14 -0800

    Update LC279 Perfect Squares

--- LC279PerfectSquares/dp.cpp
+++ LC279PerfectSquares/dp.cpp
@@ -12,19 +12,17 @@ using namespace std;
  */
 class Solution {
 public:
-    int numSquares(int n) {
+    int numSquares(int n) { // Bottom-up from n is zero
         // Primitive array type does not support variable length
         // Need to use a vector
         // Need an array of size n + 1 and filled with value n
         vector<int> dp(n + 1, n);
         dp[0] = 0; // Base case
 
-        for (int i=1; i<=n; ++i)
-        {
-            for (int j=1; j*j<=i; ++j)
-            {
-                if (i < j * j) break;
-                dp[i] = min(dp[i], dp[i - j * j] + 1);
+        for (int target = 1; target <= n; ++target) {
+            for (int j = 1; j * j <= target; ++j) { // j < sqrt(n)
+                // target - j*j is the remaining smaller problem
+                dp[target] = min(dp[target], dp[target - j * j] + 1);
             }
         }
         return dp[n];
