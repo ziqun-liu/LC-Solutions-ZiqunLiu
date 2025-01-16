70ad6a7c6d86462527a430402b7ec6c7bf50dc80
ziqun-liu <ziqun.liu@yahoo.com>
2025-01-15 17:36:20 -0800

    LC100 Same Tree

---
+++ LC0100SameTree/main.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution
+{
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q)
+    {
+        // Time complexity O(N) - visit every node once
+        // Space complexity O(N) - degenerate tree
+        if (!p && !q)
+            return true;
+        if (p && q && p->val == q->val)
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        else
+            return false;
+    }
+};
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
