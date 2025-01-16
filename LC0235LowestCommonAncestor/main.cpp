cd53c07527805f56d615daee89f6b7152866c638
ziqun-liu <ziqun.liu@yahoo.com>
2025-01-15 20:56:13 -0800

    LC235. Lowest Common Ancestor

---
+++ LC0235LowestCommonAncestor/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {
+    }
+};
+
+
+class Solution 
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        TreeNode* curr = root;
+        while (curr)
+        {
+            if (p->val > curr->val && q->val > curr->val)
+                curr = curr->right;
+            else if (p->val < curr->val && q->val < curr->val)
+                curr = curr->left;
+            else
+                return curr;
+        }
+        return nullptr;
+    }
+};
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
