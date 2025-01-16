38d58c8f83232287f52fb5023e66991284a30aaa
ziqun-liu <ziqun.liu@yahoo.com>
2025-01-15 21:40:02 -0800

    Binary Tree Level Order Traversal:

---
+++ LC0102BinaryTreeLevelOrderTraversal/LC102.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        queue = collections.deque()
+        if root:
+            queue.append(root)
+
+        while queue:
+            size = len(queue)
+            level = []
+            for _ in range(size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+
+        return result
---
+++ LC0102BinaryTreeLevelOrderTraversal/main.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {
+    }
+};
+
+
+class Solution 
+{
+    // Time complexity O(N) - each node is processed exactly once
+    // Space complexity O(N) - to keep the output structure which contains N node values
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        vector<vector<int>> result;
+        
+        queue<TreeNode*> q;
+        if (root)
+            q.push(root);
+
+        while (!q.empty())
+        {
+            vector<int> level;
+            int size = q.size();
+
+            for (int i = 0; i < size; ++i)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            result.push_back(level);
+        }
+
+        return result;
+    }
+};
+
+
+int main()
+{
+
+	return 0;
+}
