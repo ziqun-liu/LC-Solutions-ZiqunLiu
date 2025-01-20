b0910c3da9a7c331b674813a631da46e6ff6607e
ziqun-liu <ziqun.liu@yahoo.com>
2025-01-20 01:28:50 -0800

    LC211. Design Add and Search Words Data Structure

---
+++ LC0211DesignAddAndSearchWordsDataStructure/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+
+class TrieNode
+{
+public:
+    unordered_map<char, TrieNode*> children;
+    bool word;
+};
+
+
+class WordDictionary 
+{
+    TrieNode *root;
+public:
+    WordDictionary(): root(new TrieNode()) {}
+
+    void addWord(string word) 
+    {
+        TrieNode *curr = root;
+        for (char c : word)
+        {
+            if (curr->children.find(c) == curr->children.end())
+                curr->children[c] = new TrieNode();
+            curr = curr->children[c];
+        }
+        curr->word = true;
+    }
+
+    bool search(string word) 
+    {
+
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
