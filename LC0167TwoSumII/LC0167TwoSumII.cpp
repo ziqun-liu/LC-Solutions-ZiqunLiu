18372094f2adccc81410e4790678e38ce4caf9d6
ziqun-liu <ziqun.liu@yahoo.com>
2025-02-02 12:18:53 -0800

    Add LC 167.

---
+++ LC0167TwoSumII/LC0167TwoSumII.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) 
+	{
+		int l = 0, r = numbers.size();
+
+		while (l < r)
+		{
+			int currSum = numbers[l] + numbers[r];
+			if (currSum > target)
+				r--;
+			else if (currSum < target)
+				l++;
+			else
+				return { l + 1, r + 1 };;
+		}
+		return {};
+    }
+};
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
