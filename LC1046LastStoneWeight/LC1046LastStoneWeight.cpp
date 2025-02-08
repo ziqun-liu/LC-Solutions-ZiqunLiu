b98ebcb75363a745717046847bc01e4c9ef80a45
ziqun-liu <ziqun.liu@yahoo.com>
2025-02-07 22:48:52 -0800

    Add LC 1046.

---
+++ LC1046LastStoneWeight/LC1046LastStoneWeight.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<queue>
+using namespace std;
+
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> heap;
+
+        for (const int& num : stones)
+            heap.push(num);
+
+        while (heap.size() >= 2)
+        {
+            int a = heap.top();
+            heap.pop();
+            int b = heap.top();
+            heap.pop();
+            if (b < a)
+                heap.push(a - b);
+        }
+
+        heap.push(0);
+        return heap.top();
+    }
+};
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
