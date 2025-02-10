32a043e4b71532ebdc50568162a1c514ade9effb
ziqun-liu <ziqun.liu@yahoo.com>
2025-02-10 12:10:13 -0800

    Add LC 435.

---
+++ LC0435NonOverlappingIntervals/LC0435NonOverlappingIntervals.cpp
@@ -0,0 +1,36 @@
+﻿#include<algorithm>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class Solution 
+{
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) 
+	{
+		int res = 0;
+		sort(intervals.begin(), intervals.end());
+		int prevEnd = intervals[0][1];
+
+		for (int i=1; i<intervals.size(); i++)
+		{
+			if (intervals[i][0] >= prevEnd)
+				prevEnd = intervals[i][1];
+			else
+			{
+				res++;
+				prevEnd = min(prevEnd, intervals[i][1]);
+			}
+		}
+		return res;
+    }
+};
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
